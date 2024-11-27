2fd028f31f6c3746d884646f7b87c3cb67e358e5
탁정현 <tjh0147@gmail.com>
2024-11-27 09:08:42 +0900

    24.11.26.

--- workspace_java/chap00/src/chap00/Hello.java
+++ workspace_java/chap00/src/chap00/Hello.java
@@ -8,6 +8,25 @@ public class Hello {
           
 		int age;
 		double value;
+		
+		/*
+		 userinfo : 알아보기 불편하다 
+		 
+		 userInfo : 카멜 (낙타) camel case
+		 
+		 user_info: snake case
+		 
+		 user-info: 케밥 kebap case
+		 
+		 nAge
+		 sAge     : 헝가리언 표기법
+
+		 */
+
+		
+		 /*따라쓰기,안보고쓰기,응용하기,자랑하기*/
+		
+	
 	}
 
 }
