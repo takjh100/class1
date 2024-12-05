15a5c18a7d47fe74ffd326a6cc728ecce424a13e
탁정현 <tjh0147@gmail.com>
2024-12-05 17:38:00 +0900

    24.12.05

---
+++ workspace_java/chap05/src/sec01/exam01/ArrayCopyExam.java
@@ -0,0 +1,115 @@
+package sec01.exam01;
+
+import java.util.ArrayList;
+
+public class ArrayCopyExam {
+
+	public static void main(String[] args) {
+
+		//1호점에서 판매하는 메뉴
+	    String[] menu1 = new String[3];
+	    menu1[0] = "따아";
+	    menu1[1] = "말차";
+	    menu1[2] = "라떼";
+	    
+	    //2호점에서 1호점과 똑같은 메뉴를 판매할겁니다.
+	    /*
+	     * 얕은 복사
+	     * shallow copy, thin copy,call by reference
+	     * stack 영역의 값 (주소)만 복사
+	     * 값을 변경할 경우 원본이 바뀐다
+	     * 
+	     */
+		String[] menu2 = menu1;
+		//얕은복사로 연결되어있어서
+		//즉, 주소값만 공유하는 거라서
+		//복사본을 바꿔도 원본이 바뀐다
+		menu2[2] = "바나나라떼";
+		System.out.println("menu1[2] : "+menu1[2]);
+		
+		/*
+		 * 깊은 복사
+		 * deep copy, deep clone, call by value
+		 * 전혀 다른 heap영역에 똑같은 내용으로 채워놓음
+		 * 원본과 복사본은 전혀 다른 객체
+		 */
+	    String[] menu3 = new String[menu1.length];
+	    for(int i =0; i<menu1.length; i++) {
+	    	menu3[i] = menu1[i];
+	    }
+	    menu3[2] = "연유라떼";
+	    System.out.println("menu1[2] : "+ menu1[2]);
+	    System.out.println("menu3[2] : "+ menu3[2]);
+	    
+	    //3호점에서만 "사라다빵"을 추가로 팔고 싶다. 	
+		/*
+		 * 전략
+		 * menu3 보다 크기가 1개 많은 새로운 배열을 만들고
+		 * 앞부분은 깊은 복사 해놓고
+		 * 마지막에 "사라다빵"을 수동으로 추가해보자
+		 */
+	    menu3 = new String[menu1.length +1];
+	    for(int i =0; i<menu1.length; i++) {
+	    	menu3[i] = menu1[i];
+	    }
+	    menu3[menu3.length -1] = "사라다빵";
+	    for(int i =0; i<menu3.length; i++) {
+	    	System.out.println(menu3[i]);
+	    }
+	    
+	    
+	    //ArrayList 사용법
+	    ArrayList list = new ArrayList();
+	    //추가
+	    list.add(1);
+	    list.add("문자");
+	    list.add(false);
+	    
+	    //사용
+	    System.out.println(list.get(0) );
+	    System.out.println(list.get(1) );
+	    System.out.println(list.get(2) );
+	    //list를 추가한 순서대로 인덱스로 관리 
+	    
+	    //사이즈
+	    System.out.println( list.size());
+	    
+	    System.out.println(list);
+	    
+	    
+	    //향상된 for문
+	    for(String menu : menu3) {
+	    	System.out.println("meun : "+menu);
+	    }
+	    
+	    
+	    //p223 문제5
+	    //합
+	    //평균 :합/개수
+	    //최대값
+	    
+	    int[][] array = {
+	    		{95, 86},
+	    		{83,92,96},
+	    		{78,83,93,87,88}
+	   
+	    }; 
+	    
+	    int sum =0;  //합
+	    double avg = 0.0; //평균
+	    int cnt = 0; 
+
+	    for(int i=0; i<array.length; i++) {
+	    	for(int u=0; u<array[i].length; u++) {
+	    		sum += array[i][u];
+	    		cnt++;
+	    	} 
+	    }
+	    avg = (double)sum/cnt;
+	    System.out.println("합 : "+sum);
+	    System.out.println("평균 : "+avg);
+	    
+	    
+	}
+
+}
--- workspace_java/chap05/src/sec01/exam01/ArrayExam.java
+++ workspace_java/chap05/src/sec01/exam01/ArrayExam.java
@@ -88,8 +88,116 @@ public class ArrayExam {
 		 
 		int[] z = new int [10];
 		
-		for (int x= (a.length-1);x >= 0; x--) {
+		for (int x= (z.length-1);x >= 0; x--) {
+			z[x] = (x + 1);
 			System.out.println(z[x]);
+		}
+		
+		
+        
+		int[] class1 = new int[20];
+		int[] class2 = new int[20];
+		int[] class3 = new int[20];
+		int[] class4 = new int[20];
+		
+		int[][] 천안 = new int [4][20];
+		// int[][] >> int 배열의 배열이다
+        // new int [배열의수][자식배열의 수]
+		
+		System.out.println("천안 : "+천안);
+		System.out.println("천안 [0]: "+천안[0]);
+		System.out.println("천안 [0][0]: "+천안[0][0]);
+		
+		//서로 다른 크기를 가지는 2차원 배열
+		int[][] test = {
+				{1,2,3},
+				{1,2,3,4},
+				{78}
+		};
+		//두번째 배열의 마지막 값 출력
+		System.out.println(test[1][test[1].length-1]);
+		System.out.println("test.length : "+test.length);
+		System.out.println("test[0].length : "+test[0].length);
+		System.out.println("test[1].length : "+test[1].length);
+	    
+		
+		int[][] 수원 = new int [4][20];
+		int[][] 서울 = new int [4][20];
+		
+		int[][][] 휴먼 = new int [3][4][20];
+		
+		//가상의 달력
+		//12개월이고 모든 달은 30일까지만 있는
+		//한달은 1~30
+		//모든 날짜 채워 넣기
+		//따로 모든(또는 11월)출력하기
+		/*
+		 * 전략
+		 * 첫번째 배열이 12개월이고
+		 * 두번째 배열이 30일까지 로
+		 * 반복문을 사용하여 모든 날짜를 채워넣자
+		 * 
+		 */
+		
+		int[][] d = new int [12][30];
+//		int[][] 달력 = {
+//				{1{1~30},2{1~30},3{1~30},,,}
+		
+		 
+		for(int e=0;e<d.length;e++) {
+			 
+			for(int e1=0;e1<d[e].length;e1++) {
+			  System.out.println("월 : "+(e+1)+", 일 : "+(e1 +1));
+		    }
+		}
+		
+//		System.out.println("강사님풀이");
+//		int day1 = 1;
+//		int day2 = 2;
+//		
+//		int [] month1 = new int [30];
+//		for(int i=0;i<month1.length;i++) {
+//			month1[i] = i+1;
+//		}
+//		for(int i=0;i<month1.length;i++) {
+//			System.out.print(month1[i]+",");
+//		}
+//		
+//		int [] month2 = new int [12];
+//		for(int i=0;i<month1.length;i++) {
+//			month2[i] = i+1;
+//		}
+//		
+//		int[][] month = new int [12][30];
+//		for(int j=0;j<month.length;j++) {
+//			for(int i =0; i<month[j].length;i++) {
+//				month[j][i] = i+1;
+//			}
+//		}
+		
+		//랜덤
+		//3~10
+		int start = 3;
+		int end = 10;
+		
+		double dRandom = Math.random(); //0~0.9999
+		//0~7
+		int iRandom = (int)(dRandom*(end - start +1));
+		
+		int result_random = iRandom + start;
+		
+		//한줄로
+		int random = ((int)(Math.random()*(end - start +1)) ) + start;
+	
+		
+//		String[] name2 = name1; //얇은복사
+		
+		// 배열을 깊은복사
+		String[] name1 = {"김","이","박","최"};
+		//name1 와 정확히 동일한 name2를 만드세요
+		String[] name2 = new String[name1.length];
+		for(int i=0;i<name1.length;i++) {
+			name2[i] = name1[i];
 		}
 		
 		
@@ -119,6 +227,20 @@ public class ArrayExam {
 		 *  단,중복없이
 		 * 
 		 */
+		System.out.println("-----강사님1번---------");
+		int[] arr = {3,4,7,5,1,4,6};
+		int cnt = 0;
+		for(int i=0; i<arr.length;i++) {
+			if(arr[i] %2 ==1) {
+				cnt++;
+			}
+		}
+		System.out.println("1.홀수의개수 :"+cnt);
+		
+		
+		
+		
+		
 		System.out.println("-----1번---------");
 		
 		int[] z1;
@@ -126,58 +248,82 @@ public class ArrayExam {
 		z1 = new int[] {3,4,7,5,1,4,6};
 		for (int x1 =0;x1<z1.length;x1++) {
 			if (z1[x1]%2 !=0) {
-			c1 +=1;
+				c1 +=1;
 			}
-	    }    
+		}    
 		System.out.println(c1);
 		
-	    System.out.println("------2번--------");
-	  
-		int c2 = 0;
-	    for (int x1=0;x1<z1.length;x1++)
-		if(z1[x1]>4) {
-			c2 +=1;
-		}
-	    System.out.println(c2);
 		
-	    System.out.println("------3번--------");
-	    
-	    //배열된 값을 모두 출력했을 경우 비교가 가능하다
-	    //z1[x1]<z1[x1]
-	    //다음 반복값과 비교하도록
-	    
-		int max = z1[0];
-         
+		System.out.println("------강사님2번--------");
+		cnt =0;
+		for(int i=0; i<arr.length;i++) {
+			if(arr[i] >4) {
+				cnt++;
+			}
+		}
+		System.out.println("2.4보다큰 개수 :"+cnt);
+		
+		
+		
+		
+		System.out.println("------2번--------");
+		
+		int c2 = 0;
+		for (int x1=0;x1<z1.length;x1++) {
+			if(z1[x1]>4) {
+				c2 ++;
+			}
+		}
+		System.out.println(c2);
+		
+		System.out.println("------강사님3번--------");
+		int max = Integer.MIN_VALUE; //-2147483648
+		for (int i=0;i<arr.length;i++) {
+			if(max < arr[i]) {
+				max = arr[i];
+			}
+		}
+		System.out.println("최대값 : "+ max);
+		
+		
+		System.out.println("------3번--------");
+		
+		//배열된 값을 모두 출력했을 경우 비교가 가능하다
+		//z1[x1]<z1[x1]
+		//다음 반복값과 비교하도록
+		
+		max = z1[0];
+		
 		for (int x1 = 0; x1 < z1.length; x1++) {
-
+			
 			if (z1[x1] > max) {
 				max = z1[x1];
 			}
-
+			
 		}
 		System.out.println(max);
-			 
+		
 		
 		System.out.println("------4번--------");
 		int ma = z1[0];
 		int ma2 = z1[0];
 		for (int x1 = 0; x1 < z1.length; x1++) {
-
+			
 			if (z1[x1] > ma) {
 				ma = z1[x1];
-
+				
 				for (int x2 = 0; x2 < z1.length; x2++) {
-
+					
 					if (z1[x2] > ma2 && z1[x2] < ma) {
 						ma2 = z1[x2];
-
+						
 					}
 				}
-
+				
 			}
 		}
 		System.out.println(ma2);
-
+		
 		
 		System.out.println("------5번--------");
 		
@@ -197,14 +343,25 @@ public class ArrayExam {
 //			System.out.println(z1[b1]);
 			
 			System.out.println(z1[x]);
-				
-			
-			
-			
 			
 			
 		}
 		
-	}
-
+		System.out.println("------6번--------");
+//		암호 8자리 만들기
+//		반복문으로 8개의 랜덤돌리기
+		
+		
+		int[] ran = new int [8];
+		int y = 0;
+		for(int x=0;x <ran.length;x++) {
+         double ddRandom = Math.random();
+			ran[x] = (int)(ddRandom*10); 
+			System.out.println(ran[x]);
+		}
+		
+		
+		
+	}	
+ 
 }
