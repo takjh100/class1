3897a085ca09a96bbf2ea1705f09e93a78c427a6
탁정현 <tjh0147@gmail.com>
2025-02-06 19:32:20 +0900

    생산관리,생산리포팅 디자인,기능 수정완료 생산리포팅-모바일 반응형 기능추가필요 작업자 : 탁정현

--- workspace_html/project1/layOut_tak.js
+++ workspace_html/project1/layOut_tak.js
@@ -5,15 +5,15 @@ window.addEventListener('load',init)
 function init(){
 
     //메뉴 열었다 닫았다 하기<시작>
-    document.querySelector('#main-menu').addEventListener('click',function(){
+    document.querySelector('#ham').addEventListener('click',function(){
         
         
         if(document.querySelector('.mainmenu').classList.length == 1){
             document.querySelector('.mainmenu').classList.add('hide')
-            document.querySelector('#main-menu').textContent = 'MENU 열기'
+            // document.querySelector('#main-menu').textContent = 'MENU 열기'
         } else if (document.querySelector('.mainmenu').classList.length == 2){
             document.querySelector('.mainmenu').classList.remove('hide')
-            document.querySelector('#main-menu').textContent = 'MENU 닫기'
+            // document.querySelector('#main-menu').textContent = 'MENU 닫기'
         }
 
     }) //메뉴 열었다 닫았다 하기<끝>
@@ -36,11 +36,14 @@ function init(){
                 window.location.href="Production reporting_tak.html"
             } else if (e.target.textContent=='자재관리'){
                 // 자재관리 메인 
-                window.location.href="./TestMM_park/TestMM_html/TestMM_main_park.html"
+                window.location.href="TestMM_main_park.html"
             } else if (e.target.textContent=='품질관리'){
                 // 품질관리 메인 
-                window.location.href="./TestQA_park/TestQA_html/TestQA_main_park.html"
-            } 
+                window.location.href="TestQA_main_park.html"
+            } else if (e.target.textContent=='기준관리'){
+                // 기준관리 메인
+               window.location.href="masterdataManage_main_kwak.html"
+           }
         })
     }// 탑 버튼 내용나오게 하기  <끝>
 
@@ -58,10 +61,7 @@ function init(){
             } else if (e.target.textContent=='공지사항'){
                  // 공지사항 메인
                 window.location.href="noticeBoard_Nam.html"
-            }  else if (e.target.textContent=='기준관리'){
-                 // 기준관리 메인
-                window.location.href="masterdataManage_main_kwak.html"
-            }
+            }  
         })
     }// 메뉴 버튼 눌렀을때 해당 내용 나오게 하기<끝>
 
